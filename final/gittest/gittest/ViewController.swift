2294a05108e638702a7fdbdbb394e4ddf4b85eb1
Lakshmi <mandapati.l.c@gmail.com>
2020-06-11 00:11:29 -0400

    changed variable

--- final/gittest/gittest/ViewController.swift
+++ final/gittest/gittest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let msg = "example1"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
